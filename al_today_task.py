3d060093f9c944ca907faf7b4b92286a420db1c2
Admin <Admin@MacBook-Pro.local>
2023-10-24 16:24:09 +0200

    al_opirators

---
+++ al_today_task.py
@@ -0,0 +1,77 @@
+# Arithmetic_Operator_exercise 
+
+# verables_result
+
+ver_a = 1500 + 3000
+ver_b = 1500 + 3000.00
+print(ver_a + ver_b)
+
+# ver_a and ver_b are diferent type (ver_a is int, ver_b is float
+
+# ver_c object type is intger
+
+# ver_d happend error bicause ti hes two types int and str
+
+# ver_e ther are two objects type string it concantinate two strings
+
+
+# Comparision_Operator_exercise 
+
+var_a = 1500 + 3000
+var_b = 1500 + 3000.00
+var_c = "4500"
+var_d = 4500.000001
+var_e = 1000
+var_f = 5000 - 500 
+var_g = 60000
+
+
+print(var_a == var_b)
+print(var_a >= var_b)
+print(var_a > var_b)
+# print(var_a > var_c) it is error
+print(var_a > var_d)
+print(var_g < var_b)
+print(var_a != var_c)
+print(var_e == var_f)
+
+
+# Logical_Operator_exercise 
+
+print((5 > 4) and (3 == 5)) # True and True = True
+
+print((5 > 4) or (3 == 5)) # True or False = True, False or True = True
+
+
+# Indentity_Operator_exercise 
+
+my_var_a = 400
+my_var_b = '200' + '200'
+my_var_c = 400.0
+my_var_d = 200 + 200
+
+print(my_var_a == my_var_b) # There are two diferent typs: int and string
+print(my_var_a == my_var_c) # There are two diferent typs int and float but the same value
+print(my_var_a == my_var_d) # There are one type of verables
+
+
+# Bitwise_Operator_exercise 
+
+convert_number: int = 4999
+print(bin(convert_number))
+
+convert_number: int = 2111
+print(bin(convert_number))
+
+convert_number: int = 4999
+convert_number_bin: str = bin(convert_number)
+print(int(convert_number_bin, base = 2))
+
+convert_number: int = 2111
+convert_number_bin: str = bin(convert_number)
+print(int(convert_number_bin, base = 2))
+
+print(4999 & 2111)
+print(4999 | 2111)
+
+# it my task
