c72d0f5133e22879f3300e714da63aa5446421d8
Admin <Admin@MacBook-Pro.local>
2023-10-25 15:40:47 +0200

    my_condition_exesy

---
+++ statements.py
@@ -0,0 +1,42 @@
+# Control flow
+
+# sequential mode
+
+name: str = "Alex"
+age: int = 43
+amount: float = 4500.50
+total = float = age * amount
+
+
+'''
+a = 30
+b = 20
+if a < b:
+    print("is geater")
+elif a == b:
+    print("is equal")
+else:
+     print("is leser")    
+'''
+# Condition statemants
+
+if (age > 40):
+    print("you are student!")
+    if name == "Alex":
+        amount += 100.0
+    print("old amount: ", amount)
+else:    
+    print("age is: ", age)
+    print("name is: ", name)
+
+# ather example
+
+if age == 18:
+    print("welcome to Adulthood")
+elif age > 18:
+    print("our can have some beer now")
+else:
+    print("sorry no beer now")        
+
+# 
+
